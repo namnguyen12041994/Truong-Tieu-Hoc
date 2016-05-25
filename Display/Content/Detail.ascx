fe20dcac2b0e0aabca47ba603b82942de0211ac1
Administrator <Administrator@HADESKER>
2016-05-25 12:38:13 +0700

    Thêm chi tiết hiển thị

--- Display/Content/Detail.ascx
+++ Display/Content/Detail.ascx
@@ -1,2 +1,17 @@
 ﻿<%@ Control Language="C#" AutoEventWireup="true" CodeBehind="Detail.ascx.cs" Inherits="TruongTieuHoc.Display.Content.Detail" %>
 
+<div class="col-md-12 col-sm-12">
+    <asp:Repeater ID="rptChiTiet" runat="server">
+        <ItemTemplate>
+        <div class="glyphicon glyphicon-stats"></div>&nbsp;<%#: Eval("tennhom") %>
+            <hr>
+            <h3><%#: Eval("tieude") %></h3>
+           <div class="noidungchinh">
+               <%#: Eval("noidungchinh") %>
+           </div>
+            <div style="float: left"><%#: Eval("ngaydang") %></div>
+            <div style="float: right"><%#: Eval("tacgia") %></div>
+        </ItemTemplate>        
+    </asp:Repeater>
+	
+</div>
--- Display/Content/Detail.ascx.cs
+++ Display/Content/Detail.ascx.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using TruongTieuHoc.DAL;
 
 namespace TruongTieuHoc.Display.Content
 {
@@ -11,12 +12,38 @@ namespace TruongTieuHoc.Display.Content
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            
+            LoadContent(Request.QueryString["page"]);
         }
 
         void LoadContent(string query)
         {
-            
+            int idbd =0;
+            try
+            {
+                idbd = int.Parse(query);
+            }
+            catch (Exception)
+            {
+                              
+            }
+                
+            DBTruongTieuHocDataContext db = new DBTruongTieuHocDataContext();
+
+            var noidung = from ct in db.tbl_BaiDang_ChiTiets
+                join nhom in db.tbl_BaiDang_Nhoms on ct.IDNhom equals nhom.ID
+                join nguoidung in db.tbl_NguoiDungs on ct.IDNguoiDang equals nguoidung.IDAdmin
+                where ct.IDBD == idbd
+                select new
+                {
+                    tennhom = nhom.TenNhom,
+                    tieude = ct.TieuDeBD,
+                    noidungchinh = ct.NoiDungBD,
+                    ngaydang = ct.NgayDangBD,
+                    tacgia = nguoidung.Ho + " " + nguoidung.Ten
+                };
+
+            rptChiTiet.DataSource = noidung;
+            rptChiTiet.DataBind();
         }
 
     }
